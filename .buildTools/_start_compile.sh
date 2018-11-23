3a9a0f69e4b55f06e666c8a17463cde001c6354e
Viguier Benjamin <benjamin.viguier@epitech.eu>
2018-11-23 18:35:55 +0100

    Fix windows compile

---
+++ .buildTools/_start_compile.sh
@@ -0,0 +1,34 @@
+#!/usr/bin/env sh
+
+RootPath="$(
+	cd "$(dirname "$0")"
+	pwd -P
+)/..";
+cd $RootPath
+
+BuildDir="build-$(uname)"
+
+if [ "$1" = "--get-build-directory" ]; then
+	echo $BuildDir
+	exit 0
+fi
+
+if [ "$(uname)" = "Linux" ]; then
+	ConanConfigFile="linux.txt";
+	CMakeType="Unix Makefiles"
+else
+	ConanConfigFile="windows.txt";
+	CMakeType="Visual Studio 15 Win64"
+fi
+
+if [ ! -d $BuildDir ]; then
+	mkdir $BuildDir
+fi
+(
+	cd $BuildDir
+	conan remote add bincrafter https://api.bintray.com/conan/bincrafters/public-conan 2> /dev/null
+
+	conan install .. --build=missing --settings build_type=$1 --profile ../ConanPlatformSettings/$ConanConfigFile
+	cmake .. -G "$CMakeType" -DCMAKE_BUILD_TYPE=$1
+	cmake --build . ${@:2}
+) && echo "OK" || echo "KO";
