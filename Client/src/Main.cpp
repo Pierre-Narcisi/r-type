c782f2c8b16999ab78cd43834a45ff0fca261834
Loick Mury <loick.mury@epitech.eu>
2018-11-26 13:54:35 +0100

    explosion

--- Client/src/Main.cpp
+++ Client/src/Main.cpp
@@ -30,6 +30,8 @@
 #include "core/Time.hpp"
 #include "../lib/TimedEvent/TimedEventAdmin.hpp"
 #include "game/component/Parallax.hpp"
+#include "game/component/Firerate.hpp"
+#include "game/component/AnimationRtype.hpp"
 #include "game/system/Parallaxs.hpp"
 
 int main() {
@@ -44,6 +46,7 @@ int main() {
 	ecs::Ecs::addComponent<game::Parallax>(background, "assets/space.png", 100.f);
 
 	ID ship = ecs::entity::Entity::getId();
+	ecs::Ecs::addComponent<game::Firerate>(ship, 100);
 	ecs::Ecs::addComponent<ecs::component::Position>(ship, 1280/2, 720/2);
 	ecs::Ecs::addComponent<ecs::component::Speed>(ship, 10, 10);
 	ecs::Ecs::addComponent<ecs::component::Drawable>(ship, 0, true);
@@ -54,25 +57,45 @@ int main() {
 
 	ID enemy = ecs::entity::Entity::getId();
 	ecs::Ecs::addComponent<ecs::component::Position>(enemy, 1000, 720/2);
+	ecs::Ecs::addComponent<ecs::component::Speed>(enemy, 0, 0);
 	ecs::Ecs::addComponent<ecs::component::Drawable>(enemy, 0, true);
-	ecs::Ecs::addComponent<ecs::component::Hitbox>(ship, ecs::graphical::Graphic::getTextureSize("Sprite/Enemy1/Enemy11.png").x, ecs::graphical::Graphic::getTextureSize("Sprite/Enemy1/Enemy11.png").y);
+	ecs::Ecs::addComponent<ecs::component::Hitbox>(enemy, ecs::graphical::Graphic::getTextureSize("Sprite/Enemy1/Enemy11.png").x, ecs::graphical::Graphic::getTextureSize("Sprite/Enemy1/Enemy11.png").y);
 	ecs::Ecs::addComponent<ecs::component::AnimatedSprite>(enemy, "Sprite/Enemy1");
-
-
 
 	auto &keymap = ecs::Ecs::getConponentMap<ecs::component::Keyboard>()[ship].keyMap;
 	keymap[KeyKeyboard::KEY_Z] = std::pair<bool, std::function<void(ID)>>(false, [](ID self){
+		//ecs::Ecs::getConponentMap<game::AnimationRtype>()[self]. = "up";
 		ecs::Ecs::getConponentMap<ecs::component::AnimatedSpriteMap>()[self].pos = "up";
 	});
 	keymap[KeyKeyboard::KEY_S] = std::pair<bool, std::function<void(ID)>>(false, [](ID self){
 		ecs::Ecs::getConponentMap<ecs::component::AnimatedSpriteMap>()[self].pos = "down";
 	});
-	keymap[KeyKeyboard::SPACE] = std::pair<bool, std::function<void(ID)>>(false, [](ID self){
-		ID bullet = ecs::entity::Entity::getId();
-		ecs::Ecs::addComponent<ecs::component::Speed>(bullet, 10, 0);
-		ecs::Ecs::addComponent<ecs::component::Drawable>(bullet, 0, true);
-		ecs::Ecs::addComponent<ecs::component::Hitbox>(bullet, ecs::graphical::Graphic::getTextureSize("Sprite/ClassicBullet/ClassicBullet3.png").x, ecs::graphical::Graphic::getTextureSize("Sprite//ClassicBullet/ClassicBullet3.png").y);
-		ecs::Ecs::addComponent<ecs::component::AnimatedSprite>(bullet, "Sprite/ClassicBullet");
+	keymap[KeyKeyboard::SPACE] = std::pair<bool, std::function<void(ID)>>(false, [](ID parent){
+		if (std::chrono::duration_cast<std::chrono::milliseconds>(std::chrono::system_clock::now() - ecs::Ecs::getConponentMap<game::Firerate>()[parent]._lastfire).count() > ecs::Ecs::getConponentMap<game::Firerate>()[parent]._firerate)
+		{
+			TimedEventAdmin m;
+			ID bullet = ecs::entity::Entity::getId();
+			ecs::Ecs::addComponent<ecs::component::Speed>(bullet, 10, 0);
+			ecs::Ecs::addComponent<ecs::component::Drawable>(bullet, 0, true);
+			ecs::Ecs::addComponent<ecs::component::Position>(bullet, ecs::Ecs::getConponentMap<ecs::component::Position>()[parent].x, ecs::Ecs::getConponentMap<ecs::component::Position>()[parent].y);
+			ecs::Ecs::addComponent<ecs::component::Hitbox>(bullet, ecs::graphical::Graphic::getTextureSize("Sprite/ClassicBullet/ClassicBullet3.png").x, ecs::graphical::Graphic::getTextureSize("Sprite//ClassicBullet/ClassicBullet3.png").y, true, [parent](ID self, ID other){
+					if (other != parent)
+					{
+						TimedEventAdmin t;
+						ID explosion = ecs::entity::Entity::getId();
+						ecs::Ecs::addComponent<ecs::component::Speed>(explosion, 0, 0);
+						ecs::Ecs::addComponent<ecs::component::Drawable>(explosion, 0, true);
+						ecs::Ecs::addComponent<ecs::component::Position>(explosion, ecs::Ecs::getConponentMap<ecs::component::Position>()[other].x, ecs::Ecs::getConponentMap<ecs::component::Position>()[other].y);
+						ecs::Ecs::addComponent<ecs::component::AnimatedSprite>(explosion, "Sprite/Explosion", 12);
+                        ecs::Ecs::deleteId(other);
+                        ecs::Ecs::deleteId(self);
+						t.addEvent(500, Time::MilliSeconds, [explosion](){ecs::Ecs::deleteId(explosion);});
+					}
+        		});
+			ecs::Ecs::addComponent<ecs::component::Sprite>(bullet, "Sprite/ClassicBullet/ClassicBullet3.png");
+			m.addEvent(2, Time::Seconds, [bullet](){ecs::Ecs::deleteId(bullet);});
+			ecs::Ecs::getConponentMap<game::Firerate>()[parent]._lastfire = std::chrono::system_clock::now();
+		}
 	});
 
 	while (rtype.isOpen()) {
---
+++ Client/src/game/component/AnimationRtype.hpp
@@ -0,0 +1,41 @@
+#include "../game_engine/component/graphical/Sprite.hpp"
+#include <boost/filesystem.hpp>
+#include <chrono>
+
+namespace game {
+    struct AnimationRtype {
+        AnimationRtype() {}
+        AnimationRtype(std::string path, int middle, int frame)
+        {
+            _framesPerSecond = frame;
+            _actualSprite = _middle = middle;
+            std::set<std::string> sorted;
+			if (boost::filesystem::exists(path)) {
+				boost::filesystem::directory_iterator	endItr;
+				for (boost::filesystem::directory_iterator itr(path);itr != endItr;++itr) {
+					if (boost::filesystem::is_regular(itr->status())) {
+						sorted.insert(itr->path().string());
+					}
+				}
+				for (auto it = sorted.begin(); it != sorted.end(); it++) {
+					_animation.emplace_back(*it);
+				}
+			} else {
+				std::cout << "src/game_engine/component/graphical/AnimatedSprite: Missing directory \"" << path << "\"" << std::endl;
+				exit(84);
+			}
+
+			if (_animation.size() == 0) {
+				std::cout << "src/game_engine/component/graphical/AnimatedSprite: Directory must at least have 1 sprite" << std::endl;
+				exit(84);
+			}
+        }
+
+        int     _framesPerSecond;
+        int     _middle;
+        int     _actualSprite;
+
+        std::vector<ecs::component::Sprite>		_animation;
+        std::chrono::time_point<std::chrono::system_clock>     _lastchange;
+    };
+}
