0cce3bdc05d8434dfb30084748b328553d8b14e9
Loick Mury <loick.mury@epitech.eu>
2018-11-26 17:47:21 +0100

    add limit, fireate and explosion

--- Client/src/Main.cpp
+++ Client/src/Main.cpp
@@ -10,30 +10,29 @@
 #include <chrono>
 #include <thread>
 #include <zconf.h>
-#include "game_engine/ecs/Entity.hpp"
-#include "game_engine/ecs/Ecs.hpp"
-#include "game_engine/component/graphical/Sprite.hpp"
-#include "game_engine/system/graphical/Graphicals.hpp"
-#include "game_engine/component/physic/Position.hpp"
-#include "game_engine/component/control/DeplacementKeyBoard.hpp"
-#include "game_engine/component/control/Keyboard.hpp"
-#include "game_engine/component/physic/Speed.hpp"
-#include "game_engine/system/physic/Speeds.hpp"
-#include "game_engine/component/physic/Hitbox.hpp"
-#include "game_engine/component/graphical/AnimatedSprite.hpp"
-#include "game_engine/component/graphical/AnimatedSpriteMap.hpp"
-#include "game_engine/component/graphical/Drawable.hpp"
-#include "game_engine/ecs/DataBank.hpp"
-#include "game_engine/component/audio/Sound.hpp"
-#include "game_engine/component/control/DeplacementMouse.hpp"
-#include "game_engine/sfml/Graphic.hpp"
-#include "game_engine/core/Time.hpp"
+#include <ecs/Entity.hpp>
+#include <ecs/Ecs.hpp>
+#include <component/graphical/Sprite.hpp>
+#include <system/graphical/Graphicals.hpp>
+#include <component/physic/Position.hpp>
+#include <component/control/DeplacementKeyBoard.hpp>
+#include <component/control/Keyboard.hpp>
+#include <component/physic/Speed.hpp>
+#include <system/physic/Speeds.hpp>
+#include <component/physic/Hitbox.hpp>
+#include <component/graphical/AnimatedSprite.hpp>
+#include <component/graphical/AnimatedSpriteMap.hpp>
+#include <component/graphical/Drawable.hpp>
+#include <ecs/DataBank.hpp>
+#include <component/audio/Sound.hpp>
+#include <component/control/DeplacementMouse.hpp>
+#include "sfml/Graphic.hpp"
+#include "core/Time.hpp"
 #include "../lib/TimedEvent/TimedEventAdmin.hpp"
 #include "game/component/Parallax.hpp"
 #include "game/component/Firerate.hpp"
 #include "game/component/AnimationRtype.hpp"
 #include "game/system/Parallaxs.hpp"
-#include "game_engine/component/physic/Speed.hpp"
 
 int main() {
 	auto &rtype = ecs::graphical::Graphic::get();
@@ -42,6 +41,19 @@ int main() {
 		buffer.loadFromFile(path);
 		return buffer;
 	};
+	ID limitTop = ecs::entity::Entity::getId();
+	ecs::Ecs::addComponent<ecs::component::Position>(limitTop, 1280/2, 0);
+	ecs::Ecs::addComponent<ecs::component::Hitbox>(limitTop, 1280, 1, true);
+	ID limitBottom = ecs::entity::Entity::getId();
+	ecs::Ecs::addComponent<ecs::component::Position>(limitBottom, 1280/2, 720);
+	ecs::Ecs::addComponent<ecs::component::Hitbox>(limitBottom, 1280, 1, true);
+	ID limitLeft = ecs::entity::Entity::getId();
+	ecs::Ecs::addComponent<ecs::component::Position>(limitLeft, 0, 720/2);
+	ecs::Ecs::addComponent<ecs::component::Hitbox>(limitLeft, 1, 720, true);
+	ID limitRight = ecs::entity::Entity::getId();
+	ecs::Ecs::addComponent<ecs::component::Position>(limitRight, 1280, 720/2);
+	ecs::Ecs::addComponent<ecs::component::Hitbox>(limitRight, 1, 720, true);
+
 
 	ID background = ecs::entity::Entity::getId();
 	ecs::Ecs::addComponent<game::Parallax>(background, "assets/space.png", 100.f);
@@ -49,14 +61,14 @@ int main() {
 	ID ship = ecs::entity::Entity::getId();
 	ecs::Ecs::addComponent<game::Firerate>(ship, 100);
 	ecs::Ecs::addComponent<ecs::component::Position>(ship, 1280/2, 720/2);
-	ecs::Ecs::addComponent<ecs::component::Speed>(ship, 10, 10);
+	ecs::Ecs::addComponent<ecs::component::Speed>(ship, 50, 50);
 	ecs::Ecs::addComponent<ecs::component::Drawable>(ship, 0, true);
 	ecs::Ecs::addComponent<ecs::component::DeplacementKeyBoard>(ship);
-	ecs::Ecs::addComponent<ecs::component::AnimatedSpriteMap>(ship, "Sprite/Ship/BlueShip");
-	ecs::Ecs::addComponent<ecs::component::Hitbox>(ship, ecs::graphical::Graphic::getTextureSize("Sprite/Ship/BlueShip/up/BlueShip1.png").x, ecs::graphical::Graphic::getTextureSize("Sprite/Ship/BlueShip/up/BlueShip1.png").y);
-	ecs::Ecs::addComponent<ecs::component::Keyboard>(ship);
+	ecs::Ecs::addComponent<ecs::component::Sprite>(ship, "Sprite/Ship/BlueShip/BlueShip3.png");
+	ecs::Ecs::addComponent<ecs::component::Hitbox>(ship, ecs::graphical::Graphic::getTextureSize("Sprite/Ship/BlueShip/up/BlueShip1.png").x, ecs::graphical::Graphic::getTextureSize("Sprite/Ship/BlueShip/up/BlueShip1.png").y, true);
 
 	ID enemy = ecs::entity::Entity::getId();
+	ecs::Ecs::addComponent<ecs::component::Keyboard>(ship);
 	ecs::Ecs::addComponent<ecs::component::Position>(enemy, 1000, 720/2);
 	ecs::Ecs::addComponent<ecs::component::Speed>(enemy, 0, 0);
 	ecs::Ecs::addComponent<ecs::component::Drawable>(enemy, 0, true);
@@ -64,35 +76,23 @@ int main() {
 	ecs::Ecs::addComponent<ecs::component::AnimatedSprite>(enemy, "Sprite/Enemy1");
 
 	auto &keymap = ecs::Ecs::getComponentMap<ecs::component::Keyboard>()[ship].keyMap;
-	keymap[KeyKeyboard::KEY_Z] = std::pair<bool, std::function<void(ID)>>(false, [](ID self){
-		//ecs::Ecs::getComponentMap<game::AnimationRtype>()[self]. = "up";
-		ecs::Ecs::getComponentMap<ecs::component::AnimatedSpriteMap>()[self].pos = "up";
-	});
-	keymap[KeyKeyboard::KEY_S] = std::pair<bool, std::function<void(ID)>>(false, [](ID self){
-		ecs::Ecs::getComponentMap<ecs::component::AnimatedSpriteMap>()[self].pos = "down";
-	});
-	keymap[KeyKeyboard::SPACE] = std::pair<bool, std::function<void(ID)>>(false, [](ID parent){
-		if (std::chrono::duration_cast<std::chrono::milliseconds>(std::chrono::system_clock::now() -
-										  ecs::Ecs::getComponentMap<game::Firerate>()[parent]._lastfire).count() >
-			ecs::Ecs::getComponentMap<game::Firerate>()[parent]._firerate)
+	keymap[KeyKeyboard::ESCAPE] = std::pair<bool, std::function<void(ID)>>(false, [&rtype](ID parent) {rtype._window->close();});
+	keymap[KeyKeyboard::SPACE] = std::pair<bool, std::function<void(ID)>>(false, [](ID parent) {
+		if (std::chrono::duration_cast<std::chrono::milliseconds>(std::chrono::system_clock::now() - ecs::Ecs::getComponentMap<game::Firerate>()[parent]._lastfire).count() > ecs::Ecs::getComponentMap<game::Firerate>()[parent]._firerate)
 		{
 			TimedEventAdmin m;
 			ID bullet = ecs::entity::Entity::getId();
 			ecs::Ecs::addComponent<ecs::component::Speed>(bullet, 10, 0);
 			ecs::Ecs::addComponent<ecs::component::Drawable>(bullet, 0, true);
-			ecs::Ecs::addComponent<ecs::component::Position>(bullet,
-									 ecs::Ecs::getComponentMap<ecs::component::Position>()[parent].x,
-									 ecs::Ecs::getComponentMap<ecs::component::Position>()[parent].y);
-			ecs::Ecs::addComponent<ecs::component::Hitbox>(bullet, ecs::graphical::Graphic::getTextureSize("Sprite/ClassicBullet/ClassicBullet3.png").x, ecs::graphical::Graphic::getTextureSize("Sprite//ClassicBullet/ClassicBullet3.png").y, true, [parent](ID self, ID other){
+			ecs::Ecs::addComponent<ecs::component::Position>(bullet, ecs::Ecs::getComponentMap<ecs::component::Position>()[parent].x, ecs::Ecs::getComponentMap<ecs::component::Position>()[parent].y);
+			ecs::Ecs::addComponent<ecs::component::Hitbox>(bullet, ecs::graphical::Graphic::getTextureSize("Sprite/ClassicBullet/ClassicBullet3.png").x, ecs::graphical::Graphic::getTextureSize("Sprite//ClassicBullet/ClassicBullet3.png").y, false, [parent](ID self, ID other){
 					if (other != parent)
 					{
 						TimedEventAdmin t;
 						ID explosion = ecs::entity::Entity::getId();
 						ecs::Ecs::addComponent<ecs::component::Speed>(explosion, 0, 0);
 						ecs::Ecs::addComponent<ecs::component::Drawable>(explosion, 0, true);
-						ecs::Ecs::addComponent<ecs::component::Position>(explosion,
-												 ecs::Ecs::getComponentMap<ecs::component::Position>()[other].x,
-												 ecs::Ecs::getComponentMap<ecs::component::Position>()[other].y);
+						ecs::Ecs::addComponent<ecs::component::Position>(explosion, ecs::Ecs::getComponentMap<ecs::component::Position>()[other].x, ecs::Ecs::getComponentMap<ecs::component::Position>()[other].y);
 						ecs::Ecs::addComponent<ecs::component::AnimatedSprite>(explosion, "Sprite/Explosion", 12);
                         ecs::Ecs::deleteId(other);
                         ecs::Ecs::deleteId(self);
@@ -105,21 +105,16 @@ int main() {
 		}
 	});
 
-	auto &game = ecs::Ecs::get();
-	game.addUpdate(100, [&rtype](){rtype.update();});
-	game.addUpdate(1, [](){game::Parallaxs::UpdateParallaxs();});
-	game.addUpdate(10, [](){ecs::system::Controls::UpdateDeplacement();});
-	game.addUpdate(10, [](){ecs::system::Speeds::UpdateSpeeds();});
-
 	while (rtype.isOpen()) {
 		long time = ecs::core::Time::get(TimeUnit::MicroSeconds);
+		rtype.update();
+		game::Parallaxs::UpdateParallaxs();
 
-		game.update();
+		ecs::system::Controls::UpdateDeplacement();
+		ecs::system::Speeds::UpdateSpeeds();
 
 		auto x = static_cast<unsigned int>(16666 - (ecs::core::Time::get(TimeUnit::MicroSeconds) - time) > 0 ? 16666 - (ecs::core::Time::get(TimeUnit::MicroSeconds) - time) : 0);
-
-		std::cout << x << std::endl;
-		std::this_thread::sleep_for(std::chrono::microseconds(x));
+		std::this_thread::sleep_for(std::chrono::nanoseconds(x));
 	}
 
 }
