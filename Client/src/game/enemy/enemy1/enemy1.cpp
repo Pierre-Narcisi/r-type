ffa4fae19867c12013b4f95ea36c3c2ddff72b13
Seb <sebastien.le-gall@epitech.eu>
2018-11-29 18:00:51 +0100

    ECS enhance hitboxs

--- Client/src/game/enemy/enemy1/enemy1.cpp
+++ Client/src/game/enemy/enemy1/enemy1.cpp
@@ -10,11 +10,11 @@ namespace game {
 		_time = ecs::core::Time::get(TimeUnit::Seconds);
 		ecs::Ecs::addComponent<ecs::component::Drawable>(_id, 1, true);
 		ecs::Ecs::addComponent<ecs::component::AnimatedSprite>(_id, "assets/Sprite/Enemy1/", 4, ecs::core::Vector2<float>(50, 50));
-		ecs::Ecs::addComponent<ecs::component::Hitbox>(_id, _id, true, [](ID self, ID other){
+		ecs::Ecs::addComponent<ecs::component::Hitbox>(_id, _id, true, static_cast<std::function<void(ID, ID)>>([](ID self, ID other){
 			if (ecs::Ecs::idHasComponents<ecs::component::Keyboard>(other))
 				ecs::Ecs::deleteId(other);
 			ecs::Ecs::deleteId(self);
-		});
+		}));
 		ecs::Ecs::addComponent<ecs::component::Position>(_id, posx, posy);
 		ecs::Ecs::addComponent<ecs::component::Speed>(_id);
 	}
